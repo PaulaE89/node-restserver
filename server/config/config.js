1e2b64cc2f5a17822c627d2c3ee249f3ac631a48
PaulaE89 <paeb08@gmail.com>
2020-10-29 17:49:58 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://paeb08:P1a9u8l9a@cluster0.uoet3.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
